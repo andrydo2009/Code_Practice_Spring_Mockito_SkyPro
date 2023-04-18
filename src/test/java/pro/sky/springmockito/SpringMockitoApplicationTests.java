9772b3373d59b6e2ab4511b10b92efdead6354a4
andrydo2009 <115743619+andrydo2009@users.noreply.github.com>
2023-04-18 19:46:09 +0300

    Start.

---
+++ src/test/java/pro/sky/springmockito/SpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
