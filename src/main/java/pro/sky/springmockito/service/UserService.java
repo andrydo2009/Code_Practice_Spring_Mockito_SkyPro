8b1480bee4c56e8d7be54dd87d97ca0099808a82
andrydo2009 <115743619+andrydo2009@users.noreply.github.com>
2023-04-18 23:38:05 +0300

    HomeWork.

---
+++ src/main/java/pro/sky/springmockito/service/UserService.java
@@ -0,0 +1,21 @@
+package pro.sky.springmockito.service;
+
+import pro.sky.springmockito.dao.UserDaoImpl;
+import pro.sky.springmockito.model.User;
+
+
+
+public class UserService {
+
+    private final UserDaoImpl userDao=new UserDaoImpl();
+
+    public boolean checkUserExist(User userCheck){
+        return userDao.getUserByName(userCheck.getNameUser()) != null;
+    }
+    public boolean checkUserMailExist(User userCheck){
+        return userDao.getUserByMail(userCheck.getMailUser()) != null;
+    }
+
+    public void setUserDao() {
+    }
+}
---
+++ src/test/java/pro/sky/springmockito/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package pro.sky.springmockito.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mock;
+
+import org.mockito.MockitoAnnotations;
+import pro.sky.springmockito.dao.UserDaoImpl;
+import pro.sky.springmockito.model.User;
+
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+    User userTestOne = new User("SkyProThree", "skyproThree@mail.ru");
+    User userTestToo = new User("SkyPro", "skypro@mail.ru");
+
+    private UserService userService;
+
+    @Mock
+    private UserDaoImpl userDao;
+
+    @Before
+    public void setUser() {
+        MockitoAnnotations.initMocks(this);
+        userService = new UserService();
+        userService.setUserDao();
+    }
+
+
+    @Test
+    public void testCheckUserExistTrue() {
+        when(userDao.getUserByName(userTestOne.getNameUser())).thenReturn(userTestOne);
+        Assertions.assertTrue(userService.checkUserExist(userTestOne));
+    }
+    @Test
+    public void testCheckUserExistFalse() {
+        when(userDao.getUserByName(userTestToo.getNameUser())).thenReturn(userTestToo);
+        Assertions.assertFalse(userService.checkUserExist(userTestToo));
+
+    }
+    @Test
+    public void testCheckUserMailExistTrue() {
+        when(userDao.getUserByMail(userTestOne.getMailUser())).thenReturn(userTestOne);
+        Assertions.assertTrue(userService.checkUserMailExist(userTestOne));
+    }
+    @Test
+    public void testCheckUserMailExistFalse() {
+        when(userDao.getUserByMail(userTestToo.getMailUser())).thenReturn(userTestToo);
+        Assertions.assertFalse(userService.checkUserMailExist(userTestToo));
+
+    }
+
+}
